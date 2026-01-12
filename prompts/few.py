64853e1b0b2fd9c3e326d1dcd51f9bbab9c9714a
neelam1424 <neelammore1404@gmail.com>
2026-01-11 22:22:48 -0500

    Few Shot prompting

---
+++ prompts/few.py
@@ -0,0 +1,39 @@
+#Few Shot prompting
+
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client=OpenAI(
+     api_key="AIzaSyCpLTX6QsX9FDf8EPBVRA4FPvQcstg20GY",
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+#Few Shot Prompting: Directly giving the instructions to the model and few examples to the model
+SYSTEM_PROMPT="""
+You should only and only answer the coding related questions. Do not ans anything else. Your name is Alexa. If user asks something other than coding, just say sorry.  
+
+Examples:
+Q: Can you explain the a + b whole square?
+A: Sorry, I can only help with coding related questions.
+
+Q:Hey, Write a code in python for adding two numbers.
+A: def add(a,b):
+       return a + b
+"""
+
+
+response=client.chat.completions.create(
+    model="gemini-2.5-flash",
+    messages=[
+        {"role":"system","content":SYSTEM_PROMPT},
+        {"role":"user", "content":"hey, can you tell me a joke"}
+    ]
+    
+)
+
+print(response.choices[0].message.content)
+
+#2. Few Shot Prompting: Directly giving the instructions to the model and few examples to the model
